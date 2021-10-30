cd1d7bc0655e2ade7d7fdfc7ccd08bd6aaa33dcf
MaksimGritcenko <zmoaty10@gmail.com>
2021-10-30 14:47:49 +0300

    RN-6: Added everything needed for firebase connection

--- babel.config.js
+++ babel.config.js
@@ -13,6 +13,17 @@ module.exports = function(api) {
           },
         },
       ],
+      ["module:react-native-dotenv", {
+        "moduleName": "@env",
+        "path": ".env",
+        "blocklist": null,
+        "allowlist": null,
+        "blacklist": null, // DEPRECATED
+        "whitelist": null, // DEPRECATED
+        "safe": false,
+        "allowUndefined": true,
+        "verbose": false
+      }]
     ],
   };
 };
