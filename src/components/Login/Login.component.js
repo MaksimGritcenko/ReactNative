441eab5fbec0935fac99a3488f061a39f1764e05
akotikovs-sw <arturs.kotikovs@scandiweb.com>
2021-12-04 00:43:36 +0200

    RN-36. Redesigned login/register form

--- src/components/Login/Login.component.js
+++ src/components/Login/Login.component.js
@@ -8,8 +8,10 @@ import {
     SafeAreaView,
     Image,
     KeyboardAvoidingView,
-    Platform
-} from "react-native";
+    Platform, StatusBar
+} from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
 import styles from './Login.styles';
 
@@ -24,45 +26,63 @@ export const LoginComponent = (props) => {
     const [password, setPassword] = useState('');
 
     return (
-        <SafeAreaView>
+        <View>
+            <StatusBar backgroundColor={'transparent'} translucent hidden={ true }/>
             <KeyboardAvoidingView
                 behavior={ Platform.OS === 'ios' ? "padding" : "height" }
             >
                 <View style={ styles.container }>
-                    <Image style={ styles.logo } source={require('./images/skullLogo3.png')} />
                     <View style={ styles.formWrapper }>
-                        <Text style={ styles.title }>Login</Text>
-                        <TextInput
-                            style={ styles.textInput }
-                            placeholder="Email"
-                            placeholderTextColor="rgba(0,0,0,0.2)"
-                            onChangeText={text => setEmail(text) }
-                        />
-                        <TextInput
-                            style={ [styles.textInput, { marginTop: 10}] }
-                            placeholder="Password"
-                            placeholderTextColor="rgba(0,0,0,0.2)"
-                            onChangeText={ text => setPassword(text)}
-                            secureTextEntry={true}
-                        />
-                        <Text
-                            style={styles.registerLink}
-                            onPress={() => navigation.navigate('Signup')}>
-                            Doesn't have account? Click to register
-                        </Text>
-                        <Pressable
-                            style={{
-                                ...styles.button,
-                                opacity: isLoading ? .4 : 1
-                            } }
-                            onPress={ () => props.loginClick(email, password) }
-                        >
-                            <Text style={ styles.buttonContentText }>Login</Text>
-                        </Pressable>
+                        <Text style={ styles.title }>Log in</Text>
+                        <View style={{ width: '75%'}}>
+                            <View style={ styles.textInputWrapper }>
+                                <View style={ styles.icon }>
+                                    <AntDesign
+                                      name="login"
+                                      size={24}
+                                      color="black"
+                                    />
+                                </View>
+                                <TextInput
+                                  style={ styles.textInput }
+                                  placeholder="Email"
+                                  placeholderTextColor="rgba(0,0,0,0.5)"
+                                  onChangeText={text => setEmail(text) }
+                                />
+                            </View>
+                            <View style={ styles.textInputWrapper }>
+                                <View style={ styles.icon }>
+                                    <Ionicons name="lock-closed-outline" size={29} color="black" />
+                                </View>
+                                <TextInput
+                                  style={ [styles.textInput, { marginTop: 10}] }
+                                  placeholder="Password"
+                                  placeholderTextColor="rgba(0,0,0,0.5)"
+                                  onChangeText={ text => setPassword(text)}
+                                  secureTextEntry={true}
+                                />
+                            </View>
+                        </View>
+                        <View style={ styles.buttonContainer }>
+                            <Pressable
+                              style={{
+                                  ...styles.button,
+                                  opacity: isLoading ? .4 : 1
+                              } }
+                              onPress={ () => props.loginClick(email, password) }
+                            >
+                                <Text style={ styles.buttonContentText }>Log in</Text>
+                            </Pressable>
+                            <Text
+                              style={styles.registerLink}
+                              onPress={() => navigation.navigate('Signup')}>
+                                First time? Click to sign up
+                            </Text>
+                        </View>
                     </View>
                 </View>
             </KeyboardAvoidingView>
-        </SafeAreaView>
+        </View>
     )
 }
 
--- src/components/Register/Register.component.js
+++ src/components/Register/Register.component.js
@@ -12,6 +12,8 @@ import {
 } from "react-native";
 
 import styles from '../Login/Login.styles';
+import { AntDesign, Ionicons } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 
 export const RegisterComponent = (props) => {
     const { isLoading } = props;
@@ -21,7 +23,7 @@ export const RegisterComponent = (props) => {
     const [confirmPassword, setConfirmPassword] = useState('');
 
     return (
-        <SafeAreaView>
+        <View>
             <KeyboardAvoidingView
                 behavior={ Platform.OS === 'ios' ? "padding" : "height" }
             >
@@ -29,44 +31,64 @@ export const RegisterComponent = (props) => {
                     <Image style={ styles.logo } source={require('../Login/images/skullLogo3.png')} />
                     <View style={ styles.formWrapper }>
                         <Text style={ styles.title }>Registration</Text>
-                        <TextInput
-                            style={ styles.textInput }
-                            placeholder="Email"
-                            placeholderTextColor="rgba(0,0,0,0.2)"
-                            onChangeText={text => setEmail(text) }
-                        />
-                        <TextInput
-                            style={ [styles.textInput, { marginTop: 10}] }
-                            placeholder="Password"
-                            placeholderTextColor="rgba(0,0,0,0.2)"
-                            onChangeText={ text => setPassword(text)}
-                            secureTextEntry={true}
-                        />
-                        <TextInput
-                            style={ [styles.textInput, { marginTop: 10}] }
-                            placeholder="Repeat Password"
-                            placeholderTextColor="rgba(0,0,0,0.2)"
-                            onChangeText={ text => setConfirmPassword(text)}
-                            secureTextEntry={true}
-                        />
-                        <Text
-                            style={ styles.registerLink}
-                            onPress={() => props.navigation.navigate('Login')}>
-                            Already have an account? Click to login
-                        </Text>
-                        <Pressable
-                            style={{
-                                ...styles.button,
-                                opacity: isLoading ? .4 : 1
-                            } }
-                            onPress={ () => props.registerUser(email, password, confirmPassword) }
-                        >
-                            <Text style={ styles.buttonContentText }>Register</Text>
-                        </Pressable>
+                        <View style={{ width: '75%'}}>
+                            <View style={ styles.textInputWrapper }>
+                                <View style={ styles.icon }>
+                                    <Feather name="user" size={29} color="white" />
+                                </View>
+                                <TextInput
+                                  style={ styles.textInput }
+                                  placeholder="Email"
+                                  placeholderTextColor="rgba(255,255,255,.5)"
+                                  onChangeText={text => setEmail(text) }
+                                />
+                            </View>
+                            <View style={ { ...styles.textInputWrapper, flexDirection: 'column'} }>
+                                <View style={ styles.icon }>
+                                    <Ionicons name="lock-closed-outline" size={29} color="white" />
+                                </View>
+                                <TextInput
+                                  style={ [styles.textInput, { marginTop: 10}] }
+                                  placeholder="Password"
+                                  placeholderTextColor="rgba(255,255,255,.5)"
+                                  onChangeText={ text => setPassword(text)}
+                                  secureTextEntry={true}
+                                />
+                            </View>
+                            <View style={ { ...styles.textInputWrapper, flexDirection: 'column'} }>
+                                <View style={ styles.icon }>
+                                    <Ionicons name="lock-closed-outline" size={29} color="white" />
+                                </View>
+                                <TextInput
+                                  style={ [styles.textInput, { marginTop: 10}] }
+                                  placeholder="Repeat Password"
+                                  placeholderTextColor="rgba(255,255,255,5)"
+                                  onChangeText={ text => setConfirmPassword(text)}
+                                  secureTextEntry={true}
+                                />
+                            </View>
+                        </View>
+
+                        <View style={ styles.buttonContainer }>
+                            <Pressable
+                              style={{
+                                  ...styles.button,
+                                  opacity: isLoading ? .4 : 1
+                              } }
+                              onPress={ () => props.registerUser(email, password, confirmPassword) }
+                            >
+                                <Text style={ styles.buttonContentText }>Register</Text>
+                            </Pressable>
+                            <Text
+                              style={ styles.registerLink}
+                              onPress={() => props.navigation.navigate('Login')}>
+                                Already have an account? Click to login
+                            </Text>
+                        </View>
                     </View>
                 </View>
             </KeyboardAvoidingView>
-        </SafeAreaView>
+        </View>
     );
 }
 
